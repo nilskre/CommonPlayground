40e835fbb3358650b75bb7ebca0619730a208237
Celina Adam <celina.adam@gmx.net>
2018-10-31 12:38:33 +0100

    Activity Add a Session

---
+++ frontend/app/src/main/java/com/wordpress/commonplayground/commonplayground/AddSessionActivity.java
@@ -0,0 +1,13 @@
+package com.wordpress.commonplayground.commonplayground;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddSessionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_session);
+    }
+}

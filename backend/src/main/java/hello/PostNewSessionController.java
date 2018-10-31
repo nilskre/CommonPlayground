77e3d1b6736d79d0243f87d16ecf3318950088ee
Nils Krehl <nils.krehl@lherk.de>
2018-10-31 19:35:08 +0100

    request mapping function returns void

--- backend/src/main/java/hello/PostNewSessionController.java
+++ backend/src/main/java/hello/PostNewSessionController.java
@@ -1,7 +1,9 @@
 package hello;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -10,13 +12,12 @@ public class PostNewSessionController {
     private SessionList sessionList = new SessionList();
 
     @RequestMapping("/postNewSession")
-    public Session postNewSession(@RequestParam(value="name", defaultValue="not given") String name,
+    public void postNewSession(@RequestParam(value="name", defaultValue="not given") String name,
                                   @RequestParam(value="game", defaultValue = "not given") String game,
                                   @RequestParam(value="place", defaultValue = "not given") String place,
                                   @RequestParam(value="date", defaultValue = "not given") String date,
                                   @RequestParam(value="numberOfPlayers", defaultValue = "1") int numberOfPlayers) {
         Session addedSession = new Session(name, game, place, date, numberOfPlayers);
         this.sessionList.addSession(addedSession);
-        return addedSession;
     }
 }

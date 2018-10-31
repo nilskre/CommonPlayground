f286b9c50f7a533ee800a843c026534a79be193a
Nils Krehl <nils.krehl@lherk.de>
2018-10-31 19:21:08 +0100

    add Session values to request and logic

--- backend/src/main/java/hello/PostNewSessionController.java
+++ backend/src/main/java/hello/PostNewSessionController.java
@@ -7,15 +7,15 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class PostNewSessionController {
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
     private SessionList sessionList = new SessionList();
 
     @RequestMapping("/postNewSession")
-    public Session postNewSession(@RequestParam(value="name", defaultValue="World") String name, @RequestParam(value="game", defaultValue = "OneGame") String game) {
-        System.out.println("Param1: " + name);
-        System.out.println("Param2: " + game);
-        Session addedSession = new Session(name, game);
+    public Session postNewSession(@RequestParam(value="name", defaultValue="not given") String name,
+                                  @RequestParam(value="game", defaultValue = "not given") String game,
+                                  @RequestParam(value="place", defaultValue = "not given") String place,
+                                  @RequestParam(value="date", defaultValue = "not given") String date,
+                                  @RequestParam(value="numberOfPlayers", defaultValue = "1") int numberOfPlayers) {
+        Session addedSession = new Session(name, game, place, date, numberOfPlayers);
         this.sessionList.addSession(addedSession);
         return addedSession;
     }
--- backend/src/main/java/hello/Session.java
+++ backend/src/main/java/hello/Session.java
@@ -1,40 +1,29 @@
 package hello;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Session {
     private String name;
     private String game;
+    private String place;
+    private String date;
+    private int numberOfPlayers;
+    private List players  = new ArrayList();
 
-    //private final long id;
-    //private final String content;
-
-    public Session(String name, String game) {
+    public Session(String name, String game, String place, String date, int numberOfPlayers) {
         this.name = name;
         this.game = game;
-    }
+        this.place = place;
+        this.date = date;
+        this.numberOfPlayers = numberOfPlayers;
 
-    /*public Session(long id, String content) {
-        this.id = id;
-        this.content = content;
-    }*/
-
-    /*public long getId() {
-        return id;
-    }
-
-    public String getContent() {
-        return content;
-    }*/
-
-    public String getName() {
-        return name;
-    }
-
-    public String getGame() {
-        return game;
+        players.add("Host");
+        // players.size() == numberOfPlayers
     }
 
     @Override
     public String toString() {
-        return "Session: name=" + name + " game=" + game;
+        return "Session name=" + name + " game=" + game + " place=" + place + " date=" + date + " numberOfPlayers=" + numberOfPlayers + " players=" + players;
     }
 }

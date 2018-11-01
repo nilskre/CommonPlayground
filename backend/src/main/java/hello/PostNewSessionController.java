3138ca8405f4d6f5e8b8e106771dc5b14797c9f7
Nils Krehl <nils.krehl@lherk.de>
2018-11-01 19:49:30 +0100

    add unit test for controller

--- backend/src/main/java/hello/PostNewSessionController.java
+++ backend/src/main/java/hello/PostNewSessionController.java
@@ -1,22 +1,19 @@
 package hello;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class PostNewSessionController {
     private SessionList sessionList = new SessionList();
 
     @RequestMapping("/postNewSession")
-    public void postNewSession(@RequestParam(value="name", defaultValue="not given") String name,
-                                  @RequestParam(value="game", defaultValue = "not given") String game,
-                                  @RequestParam(value="place", defaultValue = "not given") String place,
-                                  @RequestParam(value="date", defaultValue = "not given") String date,
-                                  @RequestParam(value="numberOfPlayers", defaultValue = "1") int numberOfPlayers) {
+    public void postNewSession(@RequestParam(value = "name", defaultValue = "not given") String name,
+                               @RequestParam(value = "game", defaultValue = "not given") String game,
+                               @RequestParam(value = "place", defaultValue = "not given") String place,
+                               @RequestParam(value = "date", defaultValue = "not given") String date,
+                               @RequestParam(value = "numberOfPlayers", defaultValue = "1") int numberOfPlayers) {
         Session addedSession = new Session(name, game, place, date, numberOfPlayers);
         this.sessionList.addSession(addedSession);
     }

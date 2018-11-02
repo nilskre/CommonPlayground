3173e6e86574f6eac93e6889dfb284d7ff6f0a38
Celina Adam <celina.adam@gmx.net>
2018-11-02 19:07:36 +0100

    Publish gets first functions

--- frontend/app/src/main/java/com/wordpress/commonplayground/commonplayground/AddSessionActivity.java
+++ frontend/app/src/main/java/com/wordpress/commonplayground/commonplayground/AddSessionActivity.java
@@ -1,7 +1,10 @@
 package com.wordpress.commonplayground.commonplayground;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddSessionActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class AddSessionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_session);
+
+        Button publish = (Button) findViewById(R.id.ButtonPublish);
+        publish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextInputLayout title = (TextInputLayout) findViewById(R.id.TitleInput);
+                TextInputLayout game = (TextInputLayout) findViewById(R.id.GameInput);
+                TextInputLayout place = (TextInputLayout) findViewById(R.id.PlaceInput);
+                TextInputLayout time = (TextInputLayout) findViewById(R.id.TimeInput);
+                TextInputLayout players = (TextInputLayout) findViewById(R.id.PlayersInput);
+                TextInputLayout description = (TextInputLayout) findViewById(R.id.DescriptionInput);
+                
+            }
+        });
     }
 }

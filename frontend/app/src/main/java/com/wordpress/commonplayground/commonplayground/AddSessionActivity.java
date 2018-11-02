ae853179aa29656cc2182d9ea7320e3669762fc4
Celina Adam <celina.adam@gmx.net>
2018-11-02 19:35:40 +0100

    First try

--- frontend/app/src/main/java/com/wordpress/commonplayground/commonplayground/AddSessionActivity.java
+++ frontend/app/src/main/java/com/wordpress/commonplayground/commonplayground/AddSessionActivity.java
@@ -23,8 +23,25 @@ public class AddSessionActivity extends AppCompatActivity {
                 TextInputLayout time = (TextInputLayout) findViewById(R.id.TimeInput);
                 TextInputLayout players = (TextInputLayout) findViewById(R.id.PlayersInput);
                 TextInputLayout description = (TextInputLayout) findViewById(R.id.DescriptionInput);
-                
+
+                //String session = title.toString();
+                //session += game.toString();
+
             }
         });
     }
 }
+
+/**public class PostNewSessionController {
+    private SessionList sessionList = new SessionList();
+
+    @RequestMapping("/postNewSession")
+    public void postNewSession(@RequestParam(value = "name", defaultValue = "not given") String name,
+                               @RequestParam(value = "game", defaultValue = "not given") String game,
+                               @RequestParam(value = "place", defaultValue = "not given") String place,
+                               @RequestParam(value = "date", defaultValue = "not given") String date,
+                               @RequestParam(value = "numberOfPlayers", defaultValue = "1") int numberOfPlayers) {
+        Session addedSession = new Session(name, game, place, date, numberOfPlayers);
+        this.sessionList.addSession(addedSession);
+    }
+}*/

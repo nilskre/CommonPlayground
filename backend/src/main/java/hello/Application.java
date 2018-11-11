5320ad1da4751f44d976006e64dc2c6097a8137a
Nils Krehl <nils.krehl@lherk.de>
2018-11-11 21:26:29 +0100

    adapt main class

--- backend/src/main/java/hello/Application.java
+++ backend/src/main/java/hello/Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Application {
+public class Application{
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
@@ -17,26 +17,28 @@ public class Application {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     @Bean
-    public CommandLineRunner demo(SessionRepository repository) {
+    public CommandLineRunner demo(SessionRepository sessionRepository, UserRepository userRepository) {
         return (args) -> {
-            repository.save(new Session("Title INIT", "Description", "Game", "Place", "Date", 42));
+            sessionRepository.save(new Session("Title INIT", "Description", "Game", "Place", "Date", 42));
 
             // fetch all sessions
             log.info("Customers found with findAll():");
             log.info("-------------------------------");
-            for (Session session : repository.findAll()) {
+            for (Session session : sessionRepository.findAll()) {
                 log.info(session.toString());
             }
             log.info("");
 
             // fetch an individual session by ID
-            repository.findById(1L)
+            sessionRepository.findById(1L)
                     .ifPresent(customer -> {
                         log.info("Customer found with findById(1L):");
                         log.info("--------------------------------");
                         log.info(customer.toString());
                         log.info("");
                     });
+
+            userRepository.save(new User("Username"));
         };
     }
 

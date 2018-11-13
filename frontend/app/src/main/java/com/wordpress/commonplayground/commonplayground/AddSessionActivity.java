b1539660ea701da69590602ade7f0c465a2199d5
IB <inga@batton.de>
2018-11-13 16:44:28 +0100

    Code cleaned

--- frontend/app/src/main/java/com/wordpress/commonplayground/commonplayground/AddSessionActivity.java
+++ frontend/app/src/main/java/com/wordpress/commonplayground/commonplayground/AddSessionActivity.java
@@ -23,8 +23,8 @@ import java.util.Map;
 
 public class AddSessionActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button publish, btnDatePicker, btnTimePicker;
-    TextInputLayout title, game, place, date, time, numberOfPlayers, description;
+    private Button publish, btnDatePicker, btnTimePicker;
+    private TextInputLayout title, game, place, date, time, numberOfPlayers, description;
     private int mYear, mMonth, mDay, mHour, mMinute;
 
     @Override
@@ -35,10 +35,10 @@ public class AddSessionActivity extends AppCompatActivity implements View.OnClic
         publish = (Button) findViewById(R.id.ButtonPublish);
         publish.setOnClickListener(this);
 
-        btnDatePicker = (Button)findViewById(R.id.btn_date);
+        btnDatePicker = (Button) findViewById(R.id.btn_date);
         btnDatePicker.setOnClickListener(this);
 
-        btnTimePicker=(Button)findViewById(R.id.btn_time);
+        btnTimePicker = (Button) findViewById(R.id.btn_time);
         btnTimePicker.setOnClickListener(this);
 
         title = (TextInputLayout) findViewById(R.id.TitleInput);
@@ -54,86 +54,98 @@ public class AddSessionActivity extends AppCompatActivity implements View.OnClic
     @Override
     public void onClick(View v) {
         if (v == publish) {
-            /*get screen content*/
-            final String sessionTitle = title.getEditText().getText().toString();
-            final String sessionGame = game.getEditText().getText().toString();
-            final String sessionPlace = place.getEditText().getText().toString();
-            final String sessionDate = date.getEditText().getText().toString();
-            final String sessionTime = time.getEditText().getText().toString();
-            final String sessionPlayers = numberOfPlayers.getEditText().getText().toString();
-            final String sessionDesc = description.getEditText().getText().toString();
-
-            RequestQueue MyRequestQueue = Volley.newRequestQueue(this);
-            String url = "http://10.0.2.2:8080/postNewSession";
-            StringRequest MyStringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    //This code is executed if the server responds, whether or not the response contains data.
-                    //The String 'response' contains the server's response.
-                }
-            }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    //This code is executed if there is an error.
-                }
-            }) {
-                protected Map<String, String> getParams() {
-                    Map<String, String> MyData = new HashMap<String, String>();
-                    MyData.put("title", sessionTitle); //Add the data you'd like to send to the server.
-                    MyData.put("description", sessionDesc); //Add the data you'd like to send to the server.
-                    MyData.put("game", sessionGame); //Add the data you'd like to send to the server.
-                    MyData.put("place", sessionPlace); //Add the data you'd like to send to the server.
-                    MyData.put("date", sessionDate); //Add the data you'd like to send to the server.
-                    MyData.put("numberOfPlayers", sessionPlayers); //Add the data you'd like to send to the server.
-                    return MyData;
-                }
-            };
-
-            MyRequestQueue.add(MyStringRequest);
+            post();
         }
 
         if (v == btnDatePicker) {
-
-            // Get Current Date
-            final Calendar c = Calendar.getInstance();
-            mYear = c.get(Calendar.YEAR);
-            mMonth = c.get(Calendar.MONTH);
-            mDay = c.get(Calendar.DAY_OF_MONTH);
-
-            // Launch Date Picker Dialog
-            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
-                    new DatePickerDialog.OnDateSetListener() {
-
-                        @Override
-                        public void onDateSet(DatePicker view, int year,
-                                              int monthOfYear, int dayOfMonth) {
-
-                            date.getEditText().setText(((dayOfMonth >= 10)? "" : "0") + dayOfMonth + "-" + ((monthOfYear + 1 >= 10)? "" : "0") + (monthOfYear + 1) + "-" + year);
-
-                        }
-                    }, mYear, mMonth, mDay);
-            datePickerDialog.show();
+            getDate();
         }
 
         if (v == btnTimePicker) {
-
-            // Get Current Time
-            final Calendar c = Calendar.getInstance();
-            mHour = c.get(Calendar.HOUR_OF_DAY);
-            mMinute = c.get(Calendar.MINUTE);
-
-            // Launch Time Picker Dialog
-            TimePickerDialog timePickerDialog = new TimePickerDialog(this,
-                    new TimePickerDialog.OnTimeSetListener() {
-
-                        @Override
-                        public void onTimeSet(TimePicker view, int hourOfDay,
-                                              int minute) {
-
-                            time.getEditText().setText(((hourOfDay >= 10)? "" : "0") + hourOfDay + ":" + ((minute >= 10)? "" : "0") + minute);
-                        }
-                    }, mHour, mMinute, true);
-            timePickerDialog.show();
+            getTime();
         }
+
+    }
+
+    private void getTime() {
+        // Get Current Time
+        final Calendar c = Calendar.getInstance();
+        mHour = c.get(Calendar.HOUR_OF_DAY);
+        mMinute = c.get(Calendar.MINUTE);
+
+        // Launch Time Picker Dialog
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay,
+                                          int minute) {
+
+                        time.getEditText().setText(((hourOfDay >= 10) ? "" : "0") + hourOfDay + ":" + ((minute >= 10) ? "" : "0") + minute);
+                    }
+                }, mHour, mMinute, true);
+        timePickerDialog.show();
+    }
+
+    public void post() {
+        /*get screen content*/
+        final String sessionTitle = title.getEditText().getText().toString();
+        final String sessionGame = game.getEditText().getText().toString();
+        final String sessionPlace = place.getEditText().getText().toString();
+        final String sessionDate = date.getEditText().getText().toString();
+        final String sessionTime = time.getEditText().getText().toString();
+        final String sessionPlayers = numberOfPlayers.getEditText().getText().toString();
+        final String sessionDesc = description.getEditText().getText().toString();
+
+        RequestQueue MyRequestQueue = Volley.newRequestQueue(this);
+        String url = "http://10.0.2.2:8080/postNewSession";
+        StringRequest MyStringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //This code is executed if the server responds, whether or not the response contains data.
+                //The String 'response' contains the server's response.
+            }
+        }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //This code is executed if there is an error.
+            }
+        }) {
+            protected Map<String, String> getParams() {
+                Map<String, String> MyData = new HashMap<String, String>();
+                MyData.put("title", sessionTitle); //Add the data you'd like to send to the server.
+                MyData.put("description", sessionDesc); //Add the data you'd like to send to the server.
+                MyData.put("game", sessionGame); //Add the data you'd like to send to the server.
+                MyData.put("place", sessionPlace); //Add the data you'd like to send to the server.
+                MyData.put("date", sessionDate); //Add the data you'd like to send to the server.
+                MyData.put("numberOfPlayers", sessionPlayers); //Add the data you'd like to send to the server.
+                return MyData;
+            }
+        };
+
+        MyRequestQueue.add(MyStringRequest);
+    }
+
+    public void getDate() {
+        // Get Current Date
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        // Launch Date Picker Dialog
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+
+                        date.getEditText().setText(((dayOfMonth >= 10) ? "" : "0") + dayOfMonth + "-" + ((monthOfYear + 1 >= 10) ? "" : "0") + (monthOfYear + 1) + "-" + year);
+
+                    }
+                }, mYear, mMonth, mDay);
+        datePickerDialog.show();
+
     }
 }

d5a70378fb9607a9bf124c299a75b27e8361698d
Celina Adam <celina.adam@gmx.net>
2018-11-13 08:52:49 +0100

    First Espresso Test

---
+++ frontend/app/src/androidTest/java/com/wordpress/commonplayground/commonplayground/AddSessionActivityTest.java
@@ -0,0 +1,167 @@
+package com.wordpress.commonplayground.commonplayground;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class AddSessionActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void addSessionActivityTest() {
+        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.fab),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.drawer_layout),
+                                        0),
+                                2),
+                        isDisplayed()));
+        floatingActionButton.perform(click());
+
+        ViewInteraction textInputEditText = onView(
+                allOf(childAtPosition(
+                        childAtPosition(
+                                withId(R.id.TitleInput),
+                                0),
+                        0),
+                        isDisplayed()));
+        textInputEditText.perform(replaceText("Title"), closeSoftKeyboard());
+
+        ViewInteraction textInputEditText2 = onView(
+                allOf(childAtPosition(
+                        childAtPosition(
+                                withId(R.id.GameInput),
+                                0),
+                        0),
+                        isDisplayed()));
+        textInputEditText2.perform(replaceText("Game"), closeSoftKeyboard());
+
+        ViewInteraction textInputEditText3 = onView(
+                allOf(childAtPosition(
+                        childAtPosition(
+                                withId(R.id.PlaceInput),
+                                0),
+                        0),
+                        isDisplayed()));
+        textInputEditText3.perform(replaceText("Place"), closeSoftKeyboard());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.btn_date), withText("SELECT DATE"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                4),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(android.R.id.button1), withText("OK"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                3)));
+        appCompatButton2.perform(scrollTo(), click());
+
+        ViewInteraction appCompatButton3 = onView(
+                allOf(withId(R.id.btn_time), withText("SELECT TIME"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                6),
+                        isDisplayed()));
+        appCompatButton3.perform(click());
+
+        ViewInteraction appCompatButton4 = onView(
+                allOf(withId(android.R.id.button1), withText("OK"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                3)));
+        appCompatButton4.perform(scrollTo(), click());
+
+        ViewInteraction textInputEditText4 = onView(
+                allOf(childAtPosition(
+                        childAtPosition(
+                                withId(R.id.PlayersInput),
+                                0),
+                        0),
+                        isDisplayed()));
+        textInputEditText4.perform(replaceText("8"), closeSoftKeyboard());
+
+        pressBack();
+
+        ViewInteraction textInputEditText5 = onView(
+                allOf(childAtPosition(
+                        childAtPosition(
+                                withId(R.id.DescriptionInput),
+                                0),
+                        0),
+                        isDisplayed()));
+        textInputEditText5.perform(replaceText("Hello there"), closeSoftKeyboard());
+
+        pressBack();
+
+        ViewInteraction appCompatButton5 = onView(
+                allOf(withId(R.id.ButtonPublish), withText("Publish"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                9),
+                        isDisplayed()));
+        appCompatButton5.perform(click());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}

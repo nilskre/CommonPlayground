5e5adbe5055e4bfeb4ad1ec609c90266625831ec
denis <feuriegel1@web.de>
2018-11-14 16:10:54 +0100

    returned sessionList

--- backend/src/main/java/hello/SessionOverviewController.java
+++ backend/src/main/java/hello/SessionOverviewController.java
@@ -10,10 +10,22 @@ public class SessionOverviewController {
 
     private SessionRepository sessionRepository;
     private List sessions = new ArrayList();
+    private Session session1;
+    private Session session2;
 
-    @RequestMapping("/sessionList")
-    public String getSessionList() {
-        String hi= "hi";
-        return hi;
+    @RequestMapping("/getSessionList")
+    public List<Session> getSessionList() {
+
+        //session1= new Session("lol", "dkwoap", "jdwakl", "buidjwnk", "ndiwu", 23);
+        //session2= new Session("aaa", "bbbb", "cccc", "dddd", "eeee", 12);
+        //sessions.add(session1);
+        //sessions.add(session2);
+        for (Session s: sessionRepository.findAll()
+             ) {
+            System.out.println(s.toString());
+            sessions.add(s);
+        }
+
+        return sessions;
     }
 }

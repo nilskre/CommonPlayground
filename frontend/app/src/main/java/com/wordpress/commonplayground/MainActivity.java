9baa117e83bba834343cc14a0c7e2f560396e98f
Celina Adam <celina.adam@gmx.net>
2018-11-22 12:56:21 +0100

    CPG-108 Login and Registration View

--- frontend/app/src/main/java/com/wordpress/commonplayground/MainActivity.java
+++ frontend/app/src/main/java/com/wordpress/commonplayground/MainActivity.java
@@ -39,36 +39,40 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        if(true) {
+            Intent openLoginActivity = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(openLoginActivity);
+        } else {
+            setContentView(R.layout.activity_main);
 
-        RecyclerView rvSessions = (RecyclerView) findViewById(R.id.rvSessions);
-        getSessions();
+            RecyclerView rvSessions = (RecyclerView) findViewById(R.id.rvSessions);
+            getSessions();
 
-        adapter = new SessionsAdapter(activeSessions);
-        rvSessions.setAdapter(adapter);
-        rvSessions.setLayoutManager(new LinearLayoutManager(this));
+            adapter = new SessionsAdapter(activeSessions);
+            rvSessions.setAdapter(adapter);
+            rvSessions.setLayoutManager(new LinearLayoutManager(this));
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent openAddSessionActivity = new Intent(MainActivity.this, AddSessionActivity.class);
-                startActivity(openAddSessionActivity);
-            }
-        });
+            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent openAddSessionActivity = new Intent(MainActivity.this, AddSessionActivity.class);
+                    startActivity(openAddSessionActivity);
+                }
+            });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                    this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+            drawer.addDrawerListener(toggle);
+            toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
+            NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+            navigationView.setNavigationItemSelectedListener(this);
+        }
     }
 
     @Override

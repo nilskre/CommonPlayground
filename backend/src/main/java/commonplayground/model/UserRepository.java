23b0b0b28381ca51de6c6ec101ad4316d7936bcd
Nils Krehl <nils.krehl@lherk.de>
2018-11-25 21:37:38 +0100

    CPG-115 add find User by name method in Repository

--- backend/src/main/java/commonplayground/model/UserRepository.java
+++ backend/src/main/java/commonplayground/model/UserRepository.java
@@ -3,4 +3,5 @@ package commonplayground.model;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+    User findAllByName(String name);
 }

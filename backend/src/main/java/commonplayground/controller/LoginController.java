ec6df8a21e08c261e51ab72ee04430758e161d66
Nils Krehl <nils.krehl@lherk.de>
2018-11-25 21:38:48 +0100

    CPG-115 code refactoring

--- backend/src/main/java/commonplayground/controller/LoginController.java
+++ backend/src/main/java/commonplayground/controller/LoginController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
-    User userTriedToLogin;
+    private User userTriedToLogin;
 
     private final UserRepository userRepository;
     @Autowired

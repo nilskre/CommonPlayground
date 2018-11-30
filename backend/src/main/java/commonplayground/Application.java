0e986b63921c025c1766825bf8c05736b5c467ca
denis <feuriegel1@web.de>
2018-11-30 12:19:02 +0100

    fixed login again

--- backend/src/main/java/commonplayground/Application.java
+++ backend/src/main/java/commonplayground/Application.java
@@ -25,7 +25,7 @@ public class Application{
         return (args) -> {
             sessionRepository.save(new Session("Card Game", "Card fun", "Doppelkopf", "Schlosspark", "22.11.2018", 4,"12:00", Long.parseLong("1")));
             sessionRepository.save(new Session("Raid", "Raid together", "CS", "WWW", "12.12.2018", 42, "16:00", Long.parseLong("1")));
-            userRepository.save(new User("iBims", "_pswAPP89.", "test@test.de"));
+            userRepository.save(new User("iBims", "123456789", "test@test.de"));
 
             // fetch all sessions
             log.info("Customers found with findAll():");
--- backend/src/main/java/commonplayground/controller/LoginController.java
+++ backend/src/main/java/commonplayground/controller/LoginController.java
@@ -20,9 +20,9 @@ public class LoginController {
     }
 
     @RequestMapping("/login")
-    public Long postNewSession(@RequestParam(value = "email", defaultValue = "not given") String email,
-                               @RequestParam(value = "password", defaultValue = "not given") String triedPassword) {
-        userTriedToLogin = userRepository.findAllByUsername(email);
+    public Long login(@RequestParam(value = "email", defaultValue = "not given") String email,
+                      @RequestParam(value = "password", defaultValue = "not given") String triedPassword) {
+        userTriedToLogin = userRepository.findAllByEmail(email);
         if (userExists(email) && passwordCorrect(triedPassword)){
             return userTriedToLogin.getId();
         } else {
@@ -31,7 +31,7 @@ public class LoginController {
     }
 
     private boolean userExists(String email) {
-        return userRepository.findAllByEmail(email) != null;
+        return (userRepository.findAllByEmail(email) != null);
     }
 
     private boolean passwordCorrect(String triedPassword) {

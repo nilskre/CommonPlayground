50e309b7f09d1f5d4aef9fb94f109c601bda3b50
Celina Adam <celina.adam@gmx.net>
2019-04-15 14:07:19 +0200

    CPG-209 User implements interface parcelable

--- frontend/app/src/main/java/com/wordpress/commonplayground/model/User.java
+++ frontend/app/src/main/java/com/wordpress/commonplayground/model/User.java
@@ -1,11 +1,34 @@
 package com.wordpress.commonplayground.model;
 
-public class User {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class User implements Parcelable {
+
     private Long id;
     private String name;
 
-    User(String name) {
+    public User(String name) {
         this.name = name;
+    }
+
+    public User(Parcel in) {
+        super();
+        readFromParcel(in);
+    }
+
+    public static final Parcelable.Creator<User> CREATOR = new Parcelable.Creator<User>() {
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+
+    public void readFromParcel(Parcel in) {
+        name = in.readString();
     }
 
     public String getName() {
@@ -14,5 +37,15 @@ public class User {
 
     public Long getId() {
         return id;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
     }
 }

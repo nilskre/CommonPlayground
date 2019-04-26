ff193f00df753dffa69f15ab0656e5b61f79655d
Nils Krehl <nils.krehl@lherk.de>
2019-04-26 19:47:16 +0200

    CPG-231 add first unit tests and add execution in CI/CD pipeline

---
+++ frontend/app/src/test/java/com/wordpress/commonplayground/model/UserTest.java
@@ -0,0 +1,30 @@
+package com.wordpress.commonplayground.model;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+    private static User testUser;
+
+    @BeforeClass
+    public static void setUp() {
+        testUser = new User((long) 4, "Name", "test@test.de");
+    }
+
+    @Test
+    public void testId() {
+        assertEquals(0, Long.compare(testUser.getId(), 4));
+    }
+
+    @Test
+    public void testUserName() {
+        assertEquals(testUser.getName(), "Name");
+    }
+
+    @Test
+    public void testEmail() {
+        assertEquals(testUser.getEmail(), "test@test.de");
+    }
+}

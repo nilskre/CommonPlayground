eebb3a55acddafe5d8a85b53d43ea340b7a663e2
adamcel <celina.adam@gmx.net>
2019-05-09 09:15:25 +0200

    CPG-213 HEEEEEEEELP

--- frontend/app/src/androidTest/java/com/wordpress/commonplayground/test/PostSessionStepDefs.java
+++ frontend/app/src/androidTest/java/com/wordpress/commonplayground/test/PostSessionStepDefs.java
@@ -8,7 +8,6 @@ import android.widget.TimePicker;
 
 import androidx.test.espresso.contrib.PickerActions;
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.espresso.Espresso;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
@@ -35,6 +34,7 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @CucumberOptions(
         glue = "com.wordpress.commonplayground.test",
@@ -61,7 +61,7 @@ public class PostSessionStepDefs {
 
         activity = activityTestRule.getActivity();
 
-        ViewInteraction floatingActionButton = Espresso.onView(ViewMatchers.withId(R.id.fab));
+        ViewInteraction floatingActionButton = onView(withId(R.id.fab));
         floatingActionButton.perform(ViewActions.click());
     }
 
@@ -82,35 +82,35 @@ public class PostSessionStepDefs {
 
     @When("^The user types the title ([^\"]*) and the input is correct$")
     public void theUserTypesTheTitleAndTheInputIsCorrect(String testTitle) {
-        ViewInteraction textInputEditText = Espresso.onView(ViewMatchers.withId(R.id.TitleInputField));
+        ViewInteraction textInputEditText = onView(withId(R.id.TitleInputField));
         textInputEditText.perform(ViewActions.typeText(testTitle), ViewActions.closeSoftKeyboard());
     }
 
     @And("^The user types the game ([^\"]*) and the input is correct$")
     public void theUserTypesTheGameAndTheInputIsCorrect(String testGame) {
-        ViewInteraction textInputEditText = Espresso.onView(ViewMatchers.withId(R.id.GameInputField));
+        ViewInteraction textInputEditText = onView(withId(R.id.GameInputField));
         textInputEditText.perform(ViewActions.typeText(testGame), ViewActions.closeSoftKeyboard());
     }
 
     @And("^The user chooses ([^\"]*) as type$")
     public void theUserChoosesTypeAsType(String testType) {
-        ViewInteraction spinner = Espresso.onView(ViewMatchers.withId(R.id.type_spinner));
+        ViewInteraction spinner = onView(withId(R.id.type_spinner));
         spinner.perform(scrollTo(), click());
-        ViewInteraction textView = Espresso.onView(ViewMatchers.withText(testType));
+        ViewInteraction textView = onView(withText(testType));
         textView.perform(click());
     }
 
     @And("^The user chooses ([^\"]*) as genre$")
     public void theUserChoosesGenreAsGenre(String testGenre) {
-        ViewInteraction spinner = Espresso.onView(ViewMatchers.withId(R.id.genre_spinner));
+        ViewInteraction spinner = onView(withId(R.id.genre_spinner));
         spinner.perform(scrollTo(), click());
-        ViewInteraction textView = Espresso.onView(ViewMatchers.withText(testGenre));
+        ViewInteraction textView = onView(withText(testGenre));
         textView.perform(click());
     }
 
     @And("^The user types the post code ([^\"]*) and the input is correct$")
     public void theUserTypesThePlaceAndTheInputIsCorrect(String testPlace) {
-        if (!testPlace.equals("-")) {
+        if (!"-".equals(testPlace)) {
             ViewInteraction textInputEditText = onView(withId(R.id.PlaceInputField));
             textInputEditText.perform(ViewActions.typeText(testPlace), ViewActions.closeSoftKeyboard());
         }
@@ -118,55 +118,55 @@ public class PostSessionStepDefs {
 
     @And("^The user picks the date ([^\"]*)$")
     public void theUserTypesTheDateAndTheInputIsCorrect(String testDate) {
-        ViewInteraction buttonDate = Espresso.onView(ViewMatchers.withId(R.id.btn_date));
+        ViewInteraction buttonDate = onView(withId(R.id.btn_date));
         buttonDate.perform(click());
         String [] date = testDate.split("-");
         onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(Integer.parseInt(date[2]), Integer.parseInt(date[1]), Integer.parseInt(date[0])));
-        ViewInteraction buttonAccept = Espresso.onView(ViewMatchers.withText("OK"));
+        ViewInteraction buttonAccept = onView(withText("OK"));
         buttonAccept.perform(click());
     }
 
     @And("^The user picks the time ([^\"]*)$")
     public void theUserTypesTheTimeAndTheInputIsCorrect(String testTime) {
-        ViewInteraction buttonTime = Espresso.onView(ViewMatchers.withId(R.id.btn_time));
+        ViewInteraction buttonTime = onView(withId(R.id.btn_time));
         buttonTime.perform(click());
         String [] time = testTime.split(":");
         onView(withClassName(Matchers.equalTo(TimePicker.class.getName()))).perform(PickerActions.setTime(Integer.parseInt(time[0]), Integer.parseInt(time[1])));
-        ViewInteraction buttonAccept = Espresso.onView(ViewMatchers.withText("OK"));
+        ViewInteraction buttonAccept = onView(withText("OK"));
         buttonAccept.perform(click());
     }
 
     @And("^The user types the number of players ([^\"]*) and the input is correct$")
     public void theUserTypesTheNumberOfPlayersAndTheInputIsCorrect(String testPlayersNumber) {
-        ViewInteraction textInputEditText = Espresso.onView(ViewMatchers.withId(R.id.PlayersInputField));
+        ViewInteraction textInputEditText = onView(withId(R.id.PlayersInputField));
         textInputEditText.perform(ViewActions.typeText(testPlayersNumber), ViewActions.closeSoftKeyboard());
     }
 
     @And("^The user types the description ([^\"]*) and the input is correct$")
     public void theUserTypesTheDescriptionAndTheInputIsCorrect(String testDescription) {
-        ViewInteraction textInputEditText = Espresso.onView(ViewMatchers.withId(R.id.DescriptionInputField));
+        ViewInteraction textInputEditText = onView(withId(R.id.DescriptionInputField));
         textInputEditText.perform(ViewActions.typeText(testDescription), ViewActions.closeSoftKeyboard());
     }
 
     @And("^The user presses the publish button$")
     public void theUserPressesThePublishButton() {
-        ViewInteraction floatingActionButton = Espresso.onView(ViewMatchers.withId(R.id.ButtonPublish));
+        ViewInteraction floatingActionButton = onView(withId(R.id.ButtonPublish));
         floatingActionButton.perform(ViewActions.click());
     }
 
     @Then("^The posting screen is closed$")
     public void thePostingScreenIsClosed() {
-        Espresso.onView(ViewMatchers.withId(R.id.rvSessions));
+        onView(withId(R.id.rvSessions));
     }
 
     @And("^The new session with ([^\"]*) is shown$")
     public void theNewSessionWithTitleIsShown(String title) {
-        Espresso.onView(ViewMatchers.withText(title));
+        onView(withText(title));
     }
 
     @When("^The user presses the Back button$")
     public void theUserPressesTheBackButton() {
-        Espresso.onView(ViewMatchers.isRoot()).perform(ViewActions.pressBack());
+        onView(ViewMatchers.isRoot()).perform(ViewActions.pressBack());
     }
 
     @Then("^No Request is sent$")

5ecc00baed027a28ee9f12baf0585fc081ed0811
adamcel <celina.adam@gmx.net>
2019-05-09 14:04:40 +0200

    CPG-213 Add @RunWitch(Cucumber.class)

--- frontend/app/src/androidTest/java/com/wordpress/commonplayground/test/PostSessionStepDefs.java
+++ frontend/app/src/androidTest/java/com/wordpress/commonplayground/test/PostSessionStepDefs.java
@@ -20,6 +20,7 @@ import com.wordpress.commonplayground.viewmodel.SessionManager;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Rule;
+import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.java.After;
@@ -28,6 +29,7 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import cucumber.api.junit.Cucumber;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
@@ -36,6 +38,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+@RunWith(Cucumber.class)
 @CucumberOptions(
         glue = "com.wordpress.commonplayground.test",
         features = "features"

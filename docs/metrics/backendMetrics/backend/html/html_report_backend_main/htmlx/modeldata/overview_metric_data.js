00406c48f03aaef64d1d64cb7935234f32cc542a
IB <inga@batton.de>
2019-05-23 19:27:45 +0200

    codeMR metric files uploaded

---
+++ docs/metrics/backendMetrics/backend/html/html_report_backend_main/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"backend working set: {backend_main}", "Number of Highly Problematic Classes":0, "Number of Entities":22, "Number of Problematic Classes":0, "Class Lines of Code":515, "Number of External Packages":12, "Number of Packages":3, "Number of External Entities":27}

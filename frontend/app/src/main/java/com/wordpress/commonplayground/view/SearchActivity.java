2d6f717ac6a5741e26b3d70b7b4d5202e344a3af
adamcel <celina.adam@gmx.net>
2019-05-23 18:30:43 +0200

    CPG-265 Added field and spinners to Search Activity

--- frontend/app/src/main/java/com/wordpress/commonplayground/view/SearchActivity.java
+++ frontend/app/src/main/java/com/wordpress/commonplayground/view/SearchActivity.java
@@ -1,15 +1,52 @@
 package com.wordpress.commonplayground.view;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.wordpress.commonplayground.R;
 
 public class SearchActivity extends AppCompatActivity {
 
+    private Spinner type_spinner, genre_spinner;
+    private TextInputLayout placeView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        type_spinner = findViewById(R.id.type_spinner);
+        genre_spinner = findViewById(R.id.genre_spinner);
+        placeView = findViewById(R.id.PlaceInput);
+
+        type_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                updateView(i);
+            }
+
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                //do nothing
+            }
+        });
+    }
+
+    private void updateView(int item) {
+        Log.d("Selection", Integer.toString(item));
+        if (item==0){
+            placeView.setVisibility(View.GONE);
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.online_genres, android.R.layout.simple_spinner_item);
+            genre_spinner.setAdapter(adapter);
+        }else{
+            placeView.setVisibility(View.VISIBLE);
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.offline_genres, android.R.layout.simple_spinner_item);
+            genre_spinner.setAdapter(adapter);
+        }
+
     }
 }

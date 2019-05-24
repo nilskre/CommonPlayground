03c464a0fd461baf67087b28b7cc111d8a80842a
Nils Krehl <nils.krehl@lherk.de>
2019-05-24 23:50:43 +0200

    CPG-269 hotfix backend join bug

--- backend/src/main/java/commonplayground/Application.java
+++ backend/src/main/java/commonplayground/Application.java
@@ -9,15 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Application{
+public class Application {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
-    private TestData testData= new TestData();
+    private TestData testData = new TestData();
 
     public static void main(String[] args) {
         String ENV_PORT = System.getenv().get("PORT");
         String ENV_DYNO = System.getenv().get("DYNO");
-        if(ENV_PORT != null && ENV_DYNO != null) {
+        if (ENV_PORT != null && ENV_DYNO != null) {
             System.getProperties().put("server.port", ENV_PORT);
         }
         SpringApplication.run(Application.class);

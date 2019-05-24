cb5c1b2bda5342e4bd2437c83aaf2d23d16bf84d
IB <inga@batton.de>
2019-05-24 23:53:24 +0200

    codacy cleanup II

--- frontend/app/src/main/java/com/wordpress/commonplayground/network/PostLeaveRequest.java
+++ frontend/app/src/main/java/com/wordpress/commonplayground/network/PostLeaveRequest.java
@@ -10,10 +10,6 @@ import java.util.HashMap;
 
 public class PostLeaveRequest extends VolleyStringTemplate {
 
-    public PostLeaveRequest() {
-        super();
-    }
-
     @Override
     protected void handleString(String response, View view) {
         HashMap<Integer,String> snackText = new HashMap <Integer, String> (){{

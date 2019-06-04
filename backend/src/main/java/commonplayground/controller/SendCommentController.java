dd6671eb73dfa7fd92a4c5ac4f040431ad1b18c3
DRiXD <feuriegel1@web.de>
2019-06-04 23:05:52 +0200

    send COmment API works now

--- backend/src/main/java/commonplayground/controller/SendCommentController.java
+++ backend/src/main/java/commonplayground/controller/SendCommentController.java
@@ -1,26 +1,46 @@
 package commonplayground.controller;
 
+import commonplayground.model.Message;
 import commonplayground.model.MessageRepository;
+import commonplayground.model.User;
+import commonplayground.model.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class SendCommentController {
 
+    private UserRepository userRepository;
     private MessageRepository messageRepository;
+    private Message message;
+    private User receiver;
+    private int status;
 
     @Autowired
-    public SendCommentController(MessageRepository messageRepository) {
+    public SendCommentController(UserRepository userRepository, MessageRepository messageRepository) {
+         this.userRepository = userRepository;
          this.messageRepository = messageRepository;
     }
 
     @RequestMapping("/sendComment")
     public int registerNewUser(@RequestParam(value = "userID", defaultValue = "not given") String userID,
-                               @RequestParam(value = "userName", defaultValue = "not given") String userName,
+                               @RequestParam(value = "receiverName", defaultValue = "not given") String receiverName,
                                @RequestParam(value = "messageTitle", defaultValue = "not given") String messageTitle,
                                @RequestParam(value = "messageContent", defaultValue = "not given")String messageContent){
 
+        message = new Message(messageTitle, messageContent, userID);
+        receiver= userRepository.findByUsername(receiverName);
 
-
+        if(receiver != null){
+            status= 200;
+            receiver.addMessage(message);
+            messageRepository.save(message);
+        }
+        else if (receiver == null){
+            status= -300;
+        }
+        return status;
     }
 }

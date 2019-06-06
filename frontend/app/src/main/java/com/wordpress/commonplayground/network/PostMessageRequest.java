9582b143339b0b549a99c114dd4a8b28920398f3
IB-KA <39597016+ib-ka@users.noreply.github.com>
2019-06-06 17:31:42 +0200

    CPG-289 Frontend Send Message Request works preliminarily

---
+++ frontend/app/src/main/java/com/wordpress/commonplayground/network/PostMessageRequest.java
@@ -0,0 +1,24 @@
+package com.wordpress.commonplayground.network;
+
+
+import android.support.design.widget.Snackbar;
+import android.view.View;
+
+import com.wordpress.commonplayground.R;
+
+import java.util.HashMap;
+
+public class PostMessageRequest extends VolleyStringTemplate {
+
+    @Override
+    protected void handleString(String response, View view) {
+
+        HashMap<Integer, String> snackText = new HashMap<Integer, String>() {{
+            put(-300, view.getResources().getString(R.string.send_message_failure));
+            put(200, view.getResources().getString(R.string.send_message_success));
+        }};
+        Snackbar.make(view, snackText.get(Integer.parseInt(response)), 5000).show();
+
+    }
+
+}
--- frontend/app/src/main/java/com/wordpress/commonplayground/view/SendMessageActivity.java
+++ frontend/app/src/main/java/com/wordpress/commonplayground/view/SendMessageActivity.java
@@ -1,13 +1,17 @@
 package com.wordpress.commonplayground.view;
 
-import com.wordpress.commonplayground.R;
-
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.wordpress.commonplayground.R;
+import com.wordpress.commonplayground.network.PostMessageRequest;
+import com.wordpress.commonplayground.viewmodel.SessionManager;
+
+import java.util.HashMap;
 
 public class SendMessageActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -15,6 +19,7 @@ public class SendMessageActivity extends AppCompatActivity implements View.OnCli
     private EditText usernameView, titleView, messageView;
     private boolean cancel = false;
     private View focusView = null;
+    private SessionManager credentials;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,7 @@ public class SendMessageActivity extends AppCompatActivity implements View.OnCli
         usernameView = findViewById(R.id.usernameInputField);
         titleView = findViewById(R.id.messageTitleInputField);
         messageView = findViewById(R.id.messageInputField);
+        credentials = new SessionManager(this);
     }
 
     @Override
@@ -38,13 +44,22 @@ public class SendMessageActivity extends AppCompatActivity implements View.OnCli
             sendRequestToBackend();
         }
         if (!cancel) {
-            returnToMainActivity();
+            //  returnToMainActivity();
         }
     }
 
     private void sendRequestToBackend() {
-        //TODO
+
+        HashMap<String, String> parameters = new HashMap<>();
+        parameters.put("userID", credentials.getUserDetails().get(SessionManager.KEY_ID));
+        parameters.put("receiverName", usernameView.getEditableText().toString());
+        parameters.put("messageTitle", titleView.getEditableText().toString());
+        parameters.put("messageContent", messageView.getEditableText().toString());
+
+        PostMessageRequest request = new PostMessageRequest();
+        request.stringRequest("sendComment", "Send Message", parameters, this.findViewById(android.R.id.content));
     }
+
 
     private void readFields() {
         username =  usernameView.getText().toString();
@@ -81,6 +96,6 @@ public class SendMessageActivity extends AppCompatActivity implements View.OnCli
             public void run() {
                 onBackPressed();
             }
-        }, 100);
+        }, 5000);
     }
 }

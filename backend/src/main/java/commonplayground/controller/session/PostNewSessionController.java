4d23bfb223a1e74189abc77f8c49b7d69ebbd019
DRiXD <feuriegel1@web.de>
2019-06-12 18:11:36 +0200

    ficed codacy issues

--- backend/src/main/java/commonplayground/controller/session/PostNewSessionController.java
+++ backend/src/main/java/commonplayground/controller/session/PostNewSessionController.java
@@ -10,11 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 @RestController
 public class PostNewSessionController {
 

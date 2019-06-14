ba47763a79083b95f2b9eca1696089e87762003c
IB <inga@batton.de>
2019-06-14 20:24:15 +0200

    pacify codacy

--- frontend/app/src/main/java/com/wordpress/commonplayground/view/MessagesAdapter.java
+++ frontend/app/src/main/java/com/wordpress/commonplayground/view/MessagesAdapter.java
@@ -3,7 +3,6 @@ package com.wordpress.commonplayground.view;
 import android.content.res.Resources;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
